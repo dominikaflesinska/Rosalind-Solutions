6835610bdea47854be8555c01af3f162ae9de7c4
Dominika <dominikaflesinska@gmail.com>
2026-04-07 19:48:40 +0200

    Solved IPRB: Mendel's First Law

--- iprb.py
+++ iprb.py
@@ -3,15 +3,17 @@ with open('rosalind_iprb.txt', 'r') as f:
 
 total = k + m + n
 
-# P(potomek aa) - wszystkie przypadki dające aa:
+# P(offspring aa) - all cases producing aa:
 p_aa = (
-    # mm x mm: 1/4 szans na aa
+    # Aa x Aa: 1/4 chance of aa
     (m/total) * ((m-1)/(total-1)) * 0.25 +
-    # mm x aa: 1/2 szans na aa
+    # Aa x aa: 1/2 chance of aa
     (m/total) * (n/(total-1)) * 0.5 +
+    # aa x Aa: 1/2 chance of aa
     (n/total) * (m/(total-1)) * 0.5 +
-    # aa x aa: zawsze aa
+    # aa x aa: always aa
     (n/total) * ((n-1)/(total-1))
 )
 
 print(round(1 - p_aa, 5))
+
